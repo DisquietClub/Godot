889aa9492aa4ebd383e4cdeb43944316096fcf37
Leon Stansfield <leonstansfield208@gmail.com>
2024-08-16 18:00:35 +0100

    run autoformatter for godot quality checks

--- tests/scene/test_parallax_2d.h
+++ tests/scene/test_parallax_2d.h
@@ -38,81 +38,81 @@
 namespace TestParallax2D {
 
 TEST_CASE("[SceneTree][Parallax2D] Test Scroll Scale") {
-    MESSAGE("Scale test");
-    Parallax2D parallax;
-    Size2 scale(2, 2);
-    parallax.set_scroll_scale(scale);
-    CHECK(parallax.get_scroll_scale() == scale);
+	MESSAGE("Scale test");
+	Parallax2D parallax;
+	Size2 scale(2, 2);
+	parallax.set_scroll_scale(scale);
+	CHECK(parallax.get_scroll_scale() == scale);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Repeat Size") {
-    MESSAGE("Size test");
-    Parallax2D parallax;
-    Size2 size(100, 100);
-    parallax.set_repeat_size(size);
-    CHECK(parallax.get_repeat_size() == size);
+	MESSAGE("Size test");
+	Parallax2D parallax;
+	Size2 size(100, 100);
+	parallax.set_repeat_size(size);
+	CHECK(parallax.get_repeat_size() == size);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Repeat Times") {
-    MESSAGE("Times test");
-    Parallax2D parallax;
-    int times = 5;
-    parallax.set_repeat_times(times);
-    CHECK(parallax.get_repeat_times() == times);
+	MESSAGE("Times test");
+	Parallax2D parallax;
+	int times = 5;
+	parallax.set_repeat_times(times);
+	CHECK(parallax.get_repeat_times() == times);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Autoscroll") {
-    MESSAGE("Autoscroll test");
-    Parallax2D parallax;
-    Point2 autoscroll(1, 1);
-    parallax.set_autoscroll(autoscroll);
-    CHECK(parallax.get_autoscroll() == autoscroll);
+	MESSAGE("Autoscroll test");
+	Parallax2D parallax;
+	Point2 autoscroll(1, 1);
+	parallax.set_autoscroll(autoscroll);
+	CHECK(parallax.get_autoscroll() == autoscroll);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Scroll Offset") {
-    MESSAGE("Offset test");
-    Parallax2D parallax;
-    Point2 offset(10, 10);
-    parallax.set_scroll_offset(offset);
-    CHECK(parallax.get_scroll_offset() == offset);
+	MESSAGE("Offset test");
+	Parallax2D parallax;
+	Point2 offset(10, 10);
+	parallax.set_scroll_offset(offset);
+	CHECK(parallax.get_scroll_offset() == offset);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Screen Offset") {
-    MESSAGE("Screen offset test");
-    Parallax2D parallax;
-    Point2 offset(20, 20);
-    parallax.set_screen_offset(offset);
-    CHECK(parallax.get_screen_offset() == offset);
+	MESSAGE("Screen offset test");
+	Parallax2D parallax;
+	Point2 offset(20, 20);
+	parallax.set_screen_offset(offset);
+	CHECK(parallax.get_screen_offset() == offset);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Limit Begin") {
-    MESSAGE("Limit begin test");
-    Parallax2D parallax;
-    Point2 limit_begin(-100, -100);
-    parallax.set_limit_begin(limit_begin);
-    CHECK(parallax.get_limit_begin() == limit_begin);
+	MESSAGE("Limit begin test");
+	Parallax2D parallax;
+	Point2 limit_begin(-100, -100);
+	parallax.set_limit_begin(limit_begin);
+	CHECK(parallax.get_limit_begin() == limit_begin);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Limit End") {
-    MESSAGE("Limit end test");
-    Parallax2D parallax;
-    Point2 limit_end(100, 100);
-    parallax.set_limit_end(limit_end);
-    CHECK(parallax.get_limit_end() == limit_end);
+	MESSAGE("Limit end test");
+	Parallax2D parallax;
+	Point2 limit_end(100, 100);
+	parallax.set_limit_end(limit_end);
+	CHECK(parallax.get_limit_end() == limit_end);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Follow Viewport") {
-    MESSAGE("Follow viewport test");
-    Parallax2D parallax;
-    parallax.set_follow_viewport(false);
-    CHECK(parallax.get_follow_viewport() == false);
+	MESSAGE("Follow viewport test");
+	Parallax2D parallax;
+	parallax.set_follow_viewport(false);
+	CHECK(parallax.get_follow_viewport() == false);
 }
 
 TEST_CASE("[SceneTree][Parallax2D] Test Ignore Camera Scroll") {
-    MESSAGE("Ignore camera scroll test");
-    Parallax2D parallax;
-    parallax.set_ignore_camera_scroll(true);
-    CHECK(parallax.is_ignore_camera_scroll() == true);
+	MESSAGE("Ignore camera scroll test");
+	Parallax2D parallax;
+	parallax.set_ignore_camera_scroll(true);
+	CHECK(parallax.is_ignore_camera_scroll() == true);
 }
 
 } // namespace TestParallax2D
